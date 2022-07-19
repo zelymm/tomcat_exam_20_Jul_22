1a36a687ebb07ad6118bf2a6ad1d6e7a29d47daa
zelymm <dkdlwkrz@naver.com>
2022-07-20 03:05:27 +0900

    Inserted servlet library in provided form and created HelloServlet

---
+++ src/main/java/com/ll/exam/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.ll.exam;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=utf-8");
+        resp.getWriter().append("HelloServlet");
+    }
+}
